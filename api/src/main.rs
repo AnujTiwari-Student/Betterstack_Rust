ba1be0a0d1076dd8ba30954b8b43b0a2464f39b6
AnujTiwari-Student <anujavengers@gmail.com>
2025-09-11 02:12:58 +0530

    added diesel orm

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,36 @@
-use std::error::Error;
+use std::io::Error;
+
+use poem::{get, post, handler, listener::TcpListener, web::Path, web::Json, Route, Server};
+
+use crate::{request_input::CreateWebsiteInput, request_output::CreateWebsiteOutput};
+use store::Store;
+pub mod request_input;
+pub mod request_output;
+
+#[handler]
+fn get_website(Path(website_id): Path<String>) -> String {
+    format!("website: {}", website_id)
+}
+
+
+#[handler]
+fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
+    let url = data.url;
+    println!("Creating website for url: {}", url);
+    let s = Store{};
+    let id =s.create_website();
+    let response = CreateWebsiteOutput { 
+        id
+    };
+    Json(response)
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>>{
-    print!("Hello, world!");
-    Ok(())
+async fn main() -> Result<(), Error> {
+    let app = Route::new()
+        .at("/status/:website_id", get(get_website))
+        .at("/website", post(create_website));
+    Server::new(TcpListener::bind("0.0.0.0:3001"))
+      .run(app)
+      .await
 }
---
+++ store/src/lib.rs
@@ -0,0 +1,23 @@
+
+
+pub struct Store {
+    conn: Connection,
+}
+
+impl Default for Store {
+    fn default() -> Self {
+        Self {
+            conn
+        }
+    }
+}
+
+impl Store {
+    pub fn create_user(&self){
+
+    }
+
+    pub fn create_website(&self) -> String{
+        String::from("1")
+    }
+}
